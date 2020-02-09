5b260d4275a8de96cabda641dc4d800a9c8c0761
janesshasri <saravanas.office@gmail.com>
2020-02-09 15:21:21 +0530

    aSDAD

--- deleteExpenses.php
+++ deleteExpenses.php
@@ -23,54 +23,62 @@ try{
     
     $entryData = findbyand($dbcon,$bdid,'expenses','expense_no')['values'][0];
     $pastTran = findLastTrans($dbcon,$bdid,'transactions','trans_row_id');
-    $pastData = $pastTran['values'][0];
-    $entryData['payment_mode'] = $entryData['expense_paid_thru'];
-    $entryData['trans_bank'] = $entryData['expense_paid_thru']!=="Cash" ?  $entryData['expense_bank'] : "";
-
-    $entryData['payment_status'] = $entryData['expense_paid_thru']==="Cheque"?$entryData['expense_cheque_status ']==="Cleared" ? "Completed": "Uncleared" : "Completed" ;
-    $rowId = $bdid;
-    $entity = 'expenses';
-
-    $entryData['amount'] = $pastData['trans_amt'];
-    $return =  handleTransactionNew($dbcon,$entryData,$entity,$rowId,$session_org,$session_user,"reverse");
-
-    if(!$return['status']){
-        throw new Exception();
+    if(!$pastTran['status']){
+        throw new Exception();  
     }else{
-
-        echo $entryData['expense_file_src']."--";
-        if($entryData['expense_file_src']!=""){
-            if (!unlink($entryData['expense_file_src'])) {
-       
-                $sql = "DELETE FROM recordexpense WHERE id='".$_GET['id']."' ";
+        $pastData = $pastTran['values'][0];
+        $entryData['payment_mode'] = $entryData['expense_paid_thru'];
+        $entryData['trans_bank'] = $entryData['expense_paid_thru']!=="Cash" ?  $entryData['expense_bank'] : "";
     
+        $entryData['payment_status'] = $entryData['expense_paid_thru']==="Cheque"?$entryData['expense_cheque_status ']==="Cleared" ? "Completed": "Uncleared" : "Completed" ;
+        $rowId = $bdid;
+        $entity = 'expenses';
+    
+        $entryData['amount'] = $pastData['trans_amt'];
+        $return =  handleTransactionNew($dbcon,$entryData,$entity,$rowId,$session_org,$session_user,"reverse");
+    
+        if(!$return['status']){
+            throw new Exception();
+        }else{
+    
+            echo $entryData['expense_file_src']."--";
+            if($entryData['expense_file_src']!=""){
+                if (!unlink($entryData['expense_file_src'])) {
+           
+                    $sql = "DELETE FROM recordexpense WHERE id='".$_GET['id']."' ";
+        
+                    if ($dbcon->query($sql) === TRUE) {
+                    header("Location: listExpenses.php");
+                    } else {
+                        echo "Error deleting record: " . $dbcon->error;
+                    }
+                }else{
+                    throw new Exception();
+    
+                }
+            }else{
+                $sql = "DELETE FROM recordexpense WHERE id='".$_GET['id']."' ";
+        
                 if ($dbcon->query($sql) === TRUE) {
                 header("Location: listExpenses.php");
                 } else {
                     echo "Error deleting record: " . $dbcon->error;
                 }
-            }else{
-                throw new Exception();
-
             }
-        }else{
-            $sql = "DELETE FROM recordexpense WHERE id='".$_GET['id']."' ";
-    
-            if ($dbcon->query($sql) === TRUE) {
-            header("Location: listExpenses.php");
-            } else {
-                echo "Error deleting record: " . $dbcon->error;
-            }
+     
+               
         }
- 
-           
     }
+
 
 
     $dbcon->commit();
 
 }catch(Exception $e){
     $dbcon->rollback();
+    echo "Error deleting record <a href='listExpenses.php'>Back</a>";
+
+
 }finally{
     $dbcon->close();
 
